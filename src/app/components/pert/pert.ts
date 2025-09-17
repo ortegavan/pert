918b1479cb989b894eb524484bb87e561dacecc9
Van Ortega <ortegavan@live.com>
2025-09-17 17:37:55 -0300

    feat: cria componente/rota pro cálculo

---
+++ src/app/components/pert/pert.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pert',
+  imports: [],
+  templateUrl: './pert.html',
+  styleUrl: './pert.css'
+})
+export class Pert {
+
+}
